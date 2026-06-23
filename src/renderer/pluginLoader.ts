29a18007299025f9b8c50aa5d46bc36a67ebe417
David Guerizec <david@guerizec.net>
2026-06-24 01:45:44 +0200

    Convert renderer plugin loader to TypeScript

--- src/renderer/pluginLoader.ts
+++ src/renderer/pluginLoader.ts
@@ -1,22 +1,22 @@
-// @ts-check
-"use strict";
+type RendererPluginDescriptor = {
+  rendererPath?: string;
+  stylePaths?: string[];
+};
 
-/**
- * @typedef {{ rendererPath?: string, stylePaths?: string[] }} RendererPluginDescriptor
- * @typedef {{ listPlugins?: () => Promise<RendererPluginDescriptor[]> }} BoatyardRendererApi
- * @typedef {Window & { boatyard?: BoatyardRendererApi, BoatyardPluginLoader?: { ready: Promise<RendererPluginDescriptor[]> } }} PluginLoaderWindow
- */
+type PluginLoaderRendererApi = {
+  listPlugins?: () => Promise<RendererPluginDescriptor[]>;
+};
 
-/**
- * @param {PluginLoaderWindow} globalScope
- */
-(function createPluginLoader(globalScope) {
-  /**
-   * @param {string} src
-   * @returns {Promise<void>}
-   */
-  function loadScript(src) {
-    return new Promise((resolve, reject) => {
+type PluginLoaderWindow = Window & {
+  boatyard?: PluginLoaderRendererApi;
+  BoatyardPluginLoader?: {
+    ready: Promise<RendererPluginDescriptor[]>;
+  };
+};
+
+(function createPluginLoader(globalScope: PluginLoaderWindow) {
+  function loadScript(src: string) {
+    return new Promise<void>((resolve, reject) => {
       const script = document.createElement("script");
       script.src = src;
       script.async = false;
@@ -26,19 +26,13 @@
     });
   }
 
-  /**
-   * @param {string} href
-   */
-  function loadStyle(href) {
+  function loadStyle(href: string) {
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.href = href;
     document.head.append(link);
   }
 
-  /**
-   * @returns {Promise<RendererPluginDescriptor[]>}
-   */
   async function loadPlugins() {
     if (typeof globalScope.boatyard?.listPlugins !== "function") {
       return [];
@@ -62,4 +56,4 @@
   globalScope.BoatyardPluginLoader = Object.freeze({
     ready: loadPlugins()
   });
-})(/** @type {PluginLoaderWindow} */ (window));
+})(window as PluginLoaderWindow);
